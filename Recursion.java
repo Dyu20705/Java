4d71c4bbb4fa6b8054106c7b73b76eee9599728d
Dyu20705 <114639748+Dyu20705@users.noreply.github.com>
2025-01-17 19:43:20 +0700

    ComputeS.java

--- Recursion.java
+++ Recursion.java
@@ -1,32 +1,16 @@
 import java.util.*;
 
 public class Main {
-    private double x;
-    private int n;
-
-    public Main(double x, int n) {
-        this.x = x;
-        this.n = n;
-    }
-
     // Hàm đệ quy tính giá trị S
-    private double calculateS(double x, int n, int i, double currentPower, int currentFactorial, int currentPowerN) {
+    private double calculateS(double x, int n, int currentPowerN, int currentFactorial, int i) {
         if (i > n) {
-            return 1; // Giá trị cơ sở: S = 1 khi i vượt quá n
+            return 1; // Giá trị cơ sở
         }
 
-        // Tính toán giá trị hiện tại của tempx * tempn / tempi
-        double term = (currentPower * currentPowerN) / currentFactorial;
-
-        // Gọi đệ quy với i tăng lên và các giá trị được cập nhật
-        return term + calculateS(x, n, i + 1, currentPower * x, currentFactorial * (i + 1), currentPowerN * (n - 1));
+        double term = currentPowerN * Math.pow(x, i) / currentFactorial;
+        
+        return term + calculateS(x, n, currentPowerN * (n - i), currentFactorial * (i + 1), i + 1);
     }
-
-    public void solve() {
-        double result = calculateS(x, n, 1, x, 1, n);
-        System.out.println("Giá trị của S là: " + result);
-    }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -36,8 +20,8 @@ public class Main {
         System.out.print("Nhập giá trị n: ");
         int n = scanner.nextInt();
 
-        Main main = new Main(x, n);
-        main.solve();
+        Main solve = new Main();
+        System.out.println("Giá trị của S là: " + solve.calculateS(x, n, n, 1, 1));
 
         scanner.close();
     }
